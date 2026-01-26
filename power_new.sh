035d52d25a6c1a07c430ca1a63142d2695fb5060
jingqiliu <lasiwk697@outlook.com>
2026-01-26 13:09:09 -0500

    Update power_new.sh

--- power_new.sh
+++ power_new.sh
@@ -7,29 +7,37 @@
 #SBATCH --mem=8G
 #SBATCH -p standard
 #SBATCH --account=berglandlab
+
+# Number of replicates = size of array
 #SBATCH --array=1-100
+
 #SBATCH --output=/scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power/logs/power.%A_%a.out
 #SBATCH --error=/scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power/logs/power.%A_%a.err
+
 
 module purge
 module load slim
 
-TASK_ID=${SLURM_ARRAY_TASK_ID}
-RUNS_PER_TASK=50
+cd /scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power || exit 1
 
-START_REP=$(( (TASK_ID - 1) * RUNS_PER_TASK + 1 ))
-END_REP=$(( TASK_ID * RUNS_PER_TASK ))
+
+TASK_ID=${SLURM_ARRAY_TASK_ID}
+REP_ID=${TASK_ID}
 
 OUTFILE="power_chunk_${TASK_ID}.csv"
-rm -f $OUTFILE
+rm -f ${OUTFILE}
 
+echo "Starting TASK ${TASK_ID} (replicate ${REP_ID}) at $(date)"
+echo "Output file: ${OUTFILE}"
+
+## Parameters
 MODELS=("AOD" "AOD_TO")
+
 K_LIST=(300 500 1000)
 G_LIST=(10 15 20)
 S_LIST=(50 100 200)
 FOUNDERS_LIST=(10 20 40)
 
-echo "TASK $TASK_ID running replicates $START_REP to $END_REP"
 
 for MODEL in "${MODELS[@]}"; do
   for K in "${K_LIST[@]}"; do
@@ -37,23 +45,22 @@ for MODEL in "${MODELS[@]}"; do
       for S in "${S_LIST[@]}"; do
         for F in "${FOUNDERS_LIST[@]}"; do
 
-          for REP in $(seq $START_REP $END_REP); do
+          echo "RUN: REP=${REP_ID} MODEL=${MODEL} K=${K} G=${G} S=${S} FOUNDERS=${F}"
 
-            slim \
-              -d MODEL_TYPE=\"$MODEL\" \
-              -d K_CAGE=$K \
-              -d NUM_GEN=$G \
-              -d N_SAMPLE=$S \
-              -d N_FOUNDERS_HIGH=$F \
-              -d REP_ID=$REP \
-              -d OUTFILE=\"$OUTFILE\" \
-              phase3_power.slim
+          slim \
+            -d MODEL_TYPE=\"${MODEL}\" \
+            -d K_CAGE=${K} \
+            -d NUM_GEN=${G} \
+            -d N_SAMPLE=${S} \
+            -d N_FOUNDERS_HIGH=${F} \
+            -d REP_ID=${REP_ID} \
+            -d OUTFILE=\"${OUTFILE}\" \
+            phase3_power.slim
 
-          done
         done
       done
     done
   done
 done
 
-echo "TASK $TASK_ID finished"
+echo "Finished TASK ${TASK_ID} at $(date)"
