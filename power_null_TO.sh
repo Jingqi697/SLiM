c3fbd71386e421177d5cbf815a289c9b52b6d548
jingqiliu <lasiwk697@outlook.com>
2026-01-27 17:54:40 -0500

    Update power_null_TO.sh

--- power_null_TO.sh
+++ power_null_TO.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
-#SBATCH -J slim_power_AOD
-#SBATCH --ntasks-per-node=1
+#SBATCH -J slim_power
 #SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
 #SBATCH -t 24:00:00
-#SBATCH --mem=24G
+#SBATCH --mem=8G
 #SBATCH -p standard
 #SBATCH --account=berglandlab
-#SBATCH --output=/scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power/logs/power.%A.out
-#SBATCH --error=/scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power/logs/power.%A.err
+
+# Number of replicates = size of array
+#SBATCH --array=1-100
+
+#SBATCH --output=/scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power/logs/power.%A_%a.out
+#SBATCH --error=/scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power/logs/power.%A_%a.err
+
 
 module purge
 module load slim
 
-# Remove old output
-rm -f power_raw_counts.csv
+cd /scratch/cqh6wn/AOD_slim/Distribution/Inversions/1/TO/Power || exit 1
+
+
+TASK_ID=${SLURM_ARRAY_TASK_ID}
+REP_ID=${TASK_ID}
+
+OUTFILE="power_chunk_${TASK_ID}.csv"
+rm -f ${OUTFILE}
 
 ## Parameters
-
 MODELS=("NULL" "TO")
 
-K_LIST=(300 500 1000)          # cage sizes
-G_LIST=(10 15 20)             # generations
-S_LIST=(50 100 200)            # genotyped flies
-FOUNDERS_LIST=(10 20 40)       # MF founders
-
-NREP_TOTAL=100                 # total replicates
-
+K_LIST=(300 500 1000)
+G_LIST=(10 15 20)
+S_LIST=(50 100 200)
+FOUNDERS_LIST=(10 20 40)
 
 
 for MODEL in "${MODELS[@]}"; do
@@ -33,22 +41,23 @@ for MODEL in "${MODELS[@]}"; do
     for G in "${G_LIST[@]}"; do
       for S in "${S_LIST[@]}"; do
         for F in "${FOUNDERS_LIST[@]}"; do
-          for REP in $(seq 1 $NREP_TOTAL); do
 
-            echo "RUN: $MODEL K=$K G=$G S=$S Founders=$F REP=$REP"
+          echo "RUN: REP=${REP_ID} MODEL=${MODEL} K=${K} G=${G} S=${S} FOUNDERS=${F}"
 
-            slim \
-              -d MODEL_TYPE=\"$MODEL\" \
-              -d K_CAGE=$K \
-              -d NUM_GEN=$G \
-              -d N_SAMPLE=$S \
-              -d N_FOUNDERS_HIGH=$F \
-              -d REP_ID=$REP \
-              phase3_power.slim
+          slim \
+            -d MODEL_TYPE=\"${MODEL}\" \
+            -d K_CAGE=${K} \
+            -d NUM_GEN=${G} \
+            -d N_SAMPLE=${S} \
+            -d N_FOUNDERS_HIGH=${F} \
+            -d REP_ID=${REP_ID} \
+            -d OUTFILE=\"${OUTFILE}\" \
+            power_null_TO.slim
 
-          done
         done
       done
     done
   done
 done
+
+echo "Finished TASK ${TASK_ID} 
